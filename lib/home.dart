d6b6b6638c98adfff8fbbf9da317f0f291e85a99
Kwak <brettkwak@gmail.com>
2024-09-09 04:13:51 +0900

    feat : added status table

--- lib/home.dart
+++ lib/home.dart
@@ -6,7 +6,55 @@ class home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Home screen",
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("나의 신고 처리 현황"),
+            Table(
+              children: <TableRow> [
+                TableRow(
+                  children: [
+                    Center(
+                      child: Text("총 신고 건수"),
+                    ),
+                    Center(
+                      child: Text("진행"),
+                    ),
+                    Center(
+                      child: Text("취하"),
+                    ),
+                    Center(
+                      child: Text("답변완료"),
+                    ),
+                  ]
+                ),
+                TableRow(
+                  children: [
+                    Center(
+                      child: Text("00건"),
+                    ),
+                    Center(
+                      child: Text("0건"),
+                    ),
+                    Center(
+                      child: Text("0건"),
+                    ),
+                    Center(
+                      child: Text("00건"),
+                    ),
+                  ]
+                ),
+              ],
+            ),
 
+          ],
+        ),
+      ),
     );
   }
+
+
+
 }
