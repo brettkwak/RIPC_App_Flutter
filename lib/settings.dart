e75a9c49bf253c3d227120e47c9441068ce92ef1
Kwak <brettkwak@gmail.com>
2024-09-19 04:02:25 +0900

    feat : profile, buttons

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,12 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:ripc_flutter/loginscreen.dart';
+import 'package:ripc_flutter/user_data.dart';
+import 'login_control.dart';
 
 class settings extends StatelessWidget {
   const settings({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
+    String? email = user_data().email;
+    String? profile_image_url = user_data().profile_image_url;
 
+    return MaterialApp(
+      title: 'Settings',
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            Container(
+              height: 100,
+              color: Colors.white,
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 80,
+                ),
+                CircleAvatar(
+                  radius: 30,
+                  child: ClipOval(
+                    child: Image.network('$profile_image_url'),
+                  ),
+                ),
+                Text('$email')
+              ],
+            ),
+            Container(
+              height: 50,
+            ),
+            OutlinedButton(
+              child: Text('About'),
+              onPressed: (){
+
+              },
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.black,
+              ),
+            ),
+            OutlinedButton(
+              child: Text('Logout'),
+              onPressed: (){
+                LoginAPI.logOut;
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => loginscreen(),
+                  ),
+                );
+              },
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.red,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
