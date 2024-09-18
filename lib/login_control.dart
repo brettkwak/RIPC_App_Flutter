50ac41310c3a10abb34a9eaeeb565a902899f8a0
Kwak <brettkwak@gmail.com>
2024-09-19 04:06:00 +0900

    feat : separated login process

---
+++ lib/login_control.dart
@@ -0,0 +1,7 @@
+import 'package:google_sign_in/google_sign_in.dart';
+
+class LoginAPI {
+  static final _googleSignIn = GoogleSignIn();
+  static Future<GoogleSignInAccount?> login() => _googleSignIn.signIn();
+  static Future logOut = _googleSignIn.signOut();
+}
