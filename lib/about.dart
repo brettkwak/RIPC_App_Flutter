55a8576a0e4e1edf951d8bcf59a964d187de0814
Kwak <brettkwak@gmail.com>
2024-09-22 04:44:29 +0900

    added about page

---
+++ lib/about.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class about extends StatelessWidget {
+  const about({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'about page',
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+        ),
+      ),
+    );
+  }
+}
