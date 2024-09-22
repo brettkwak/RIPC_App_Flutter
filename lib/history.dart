3b367463baf0b9fd5a14f6a3dd0f8e23243b5f76
Kwak <brettkwak@gmail.com>
2024-09-22 22:30:04 +0900

    feat : added date picking

--- lib/history.dart
+++ lib/history.dart
@@ -1,10 +1,58 @@
 import 'package:flutter/material.dart';
 
-class history extends StatelessWidget {
-  const history({super.key});
+class history extends StatefulWidget {
+  // const ({super.key});
+
+  @override
+  State<history> createState() => _historyState();
+}
+
+class _historyState extends State<history> {
+
+  TextEditingController _dateController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(30),
+          child: TextField(
+            controller: _dateController,
+            decoration: InputDecoration(
+              labelText: 'DATE',
+              filled: true,
+              prefixIcon: Icon(Icons.calendar_today),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide.none
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue)
+              )
+            ),
+            readOnly: true,
+            onTap: (){
+              _selectDate();
+            },
+          ),
+        ),
+      ),
+    );
   }
+
+  Future<void> _selectDate() async{
+    DateTime? _picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2100)
+    );
+
+    if (_picked != null){
+      setState(() {
+        _dateController.text = _picked.toString().split(" ")[0];
+      });
+    }
+  }
+
 }
