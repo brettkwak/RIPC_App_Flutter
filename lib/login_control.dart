4759336674f139da53a0c52f6e2b34e7098ea4a9
Kwak <brettkwak@gmail.com>
2024-09-22 22:28:39 +0900

    feat : logged-in check

--- lib/login_control.dart
+++ lib/login_control.dart
@@ -1,7 +1,26 @@
 import 'package:google_sign_in/google_sign_in.dart';
+import 'user_data.dart';
 
 class LoginAPI {
   static final _googleSignIn = GoogleSignIn();
-  static Future<GoogleSignInAccount?> login() => _googleSignIn.signIn();
   static Future logOut = _googleSignIn.signOut();
+}
+
+Future<void> loginProcess() async {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  GoogleSignInAccount? _currentUser = _googleSignIn.currentUser;
+
+  if (_currentUser == null){
+    _currentUser = await _googleSignIn.signIn();
+  }
+
+  user_data().email = _currentUser?.email;
+  user_data().profile_image_url = _currentUser?.photoUrl;
+  user_data().user_name = _currentUser?.displayName;
+  user_data().user_id = _currentUser?.id;
+  // print(_currentUser?.displayName);
+  // print(_currentUser?.email);
+  // print(_currentUser?.id);
+  // print(_currentUser?.photoUrl);
+  return;
 }
