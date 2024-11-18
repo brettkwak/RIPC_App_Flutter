e8dfc009caf47e41166e79df010e42493de36ad4
Kwak <brettkwak@gmail.com>
2024-11-19 05:00:55 +0900

    added gps screen

---
+++ lib/gps.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LocationScreen extends StatefulWidget {
+  const LocationScreen({super.key});
+
+  @override
+  State<LocationScreen> createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'gps screen',
+      home: Scaffold(
+
+      ),
+    );
+  }
+}
